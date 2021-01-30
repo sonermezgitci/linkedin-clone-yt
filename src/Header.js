0004af8759c826e499d4107038283b46a543f4fe
Soner Mezgitci <mezgitci9@gmail.com>
2021-01-30 00:50:46 -0500

    HeaderOption.css & Header.css implemented

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import './Header.css'
-import HeaderOption from "./HeaderOption"
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from "./HeaderOption"
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+
+
 
 function Header() {
     return (
@@ -17,9 +25,12 @@ function Header() {
                 </div>
 
             </div>
-            <div className=".header__right">
-                <HeaderOption title="Home" />
-                <HeaderOption title="My Network" />
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
             </div>
 
         </div>

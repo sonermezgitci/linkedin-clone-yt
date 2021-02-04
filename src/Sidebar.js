964a0d15760514fc90934f84e0b91511e2a9689d
Soner Mezgitci <mezgitci9@gmail.com>
2021-02-03 23:57:46 -0500

    SideBar Hash added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,15 @@ import "./Sidebar.css";
 import { Avatar } from "@material-ui/core"
 
 function Sidebar() {
+    const recentItem = (topic) => {
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash"></span>
+            <p>{topic}</p>
+        </div>
+    };
+
+
+
     return (
         <div className="sidebar">
             <div className="sidebar__top">
@@ -25,8 +34,15 @@ function Sidebar() {
 
 
             </div>
-            <div className="sidebar__bottom "></div>
-            <p> Recent</p>
+            <div className="sidebar__bottom ">
+                <p> Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('data')}
+                {recentItem('design')}
+
+            </div>
         </div>
     )
 }

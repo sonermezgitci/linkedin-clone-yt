2c75eb23f8b1475d3b9a6cdfe73f07d5c520512d
Soner Mezgitci <mezgitci9@gmail.com>
2021-02-07 23:59:21 -0500

    Input backgeroundd

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./Feed.css"
 import CreateIcon from "@material-ui/icons/Create";
-import InputOption from "./InputOption"
 import ImageIcon from "@material-ui/icons/Image"
+import InputOption from "./InputOption";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions"
 import EventNoteIcon from "@material-ui/icons/EventNote"
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay"
@@ -23,7 +23,7 @@ function Feed() {
                     <InputOption Icon={ImageIcon} title="Photo" color="#7085F9" />
                     <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
                     <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBC0" />
-                    <InputOption Icon={CalendarViewDayIcon} title="Write artcile" color="7FC15E" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="7FC15E" />
                 </div>
             </div>
         </div>
